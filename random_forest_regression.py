1ea86e259dad534e214482e625455e707267aba9
Sherif Atitebi <sherifatitebi@gmail.com>
2022-01-19 14:50:21 +0000

    Random forest regression

---
+++ random_forest_regression.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 19 14:10:07 2022
+
+@author: SHERIF ATITEBI O
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+
+dataset = pd.read_csv("Position_Salaries.csv")
+x = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+
+#Training the Random Forest model
+# n_estimators is number of trees
+regressor = RandomForestRegressor(n_estimators= 10, random_state=0)
+regressor.fit(x, y)
+
+print(regressor.predict([[6.5]]))
+
+X_grid = np.arange(min(x), max(x), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(x, y, color="red")
+plt.plot(X_grid, regressor.predict(X_grid), color="blue")
+plt.title("Truth or Bluff (Random Forest Regression)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.show()
