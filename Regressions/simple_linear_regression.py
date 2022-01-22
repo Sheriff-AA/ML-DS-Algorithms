de1b8178ce2b4ef965abd79c397cc5bbb6da54ea
Sherif Atitebi <sherifatitebi@gmail.com>
2022-01-22 01:56:56 +0000

    Logistic Regression

--- Regressions/simple_linear_regression.py
+++ Regressions/simple_linear_regression.py
@@ -21,7 +21,7 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_
 
 # TRAINING THE SIMPLE LINEAR REGRESSION MODEL ON TRAINING SET
 regressor = LinearRegression()
-regressor.fit(x_train,  y_train)
+regressor.fit(x_train, y_train)
 
 # PREDICTING THE TEST RESULT
 y_pred = regressor.predict(x_test)
