0a289ba79b3f92bbabeb1ad87f2c36708f854402
Sherif Atitebi <sherifatitebi@gmail.com>
2022-02-06 13:57:38 +0000

    Hierachical clustering

--- Classification/naive_bayes.py
+++ Classification/naive_bayes.py
@@ -7,7 +7,6 @@ Created on Sun Feb  6 08:32:00 2022
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.naive_bayes import GaussianNB
--- Clustering/hierachical_clustering.py
+++ Clustering/hierachical_clustering.py
@@ -5,3 +5,35 @@ Created on Sun Feb  6 11:01:11 2022
 @author: SHERIF ATITEBI O
 """
 
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy.cluster.hierarchy as sch
+from sklearn.cluster import AgglomerativeClustering
+#%%
+
+dataset = pd.read_csv("Mall_Customers.csv")
+x = dataset.iloc[:, [3, 4]].values
+
+#Using the dendogram to find the optimum number of clusters
+dendogram = sch.dendrogram(sch.linkage(x, method="ward"))
+
+plt.title("Dendrogram")
+plt.xlabel("Customers")
+plt.ylabel("Euclidean Distances")
+plt.show()
+#%%
+hc = AgglomerativeClustering(n_clusters=5, affinity="euclidean", linkage="ward")
+y_hc = hc.fit_predict(x)
+print(y_hc)
+
+#%%
+plt.scatter(x[y_hc == 0, 0], x[y_hc == 0, 1], s = 100, c = "red", label="Cluster 1")
+plt.scatter(x[y_hc == 1, 0], x[y_hc == 1, 1], s = 100, c = "blue", label="Cluster 2")
+plt.scatter(x[y_hc == 2, 0], x[y_hc == 2, 1], s = 100, c = "green", label="Cluster 3")
+plt.scatter(x[y_hc == 3, 0], x[y_hc == 3, 1], s = 100, c = "cyan", label="Cluster 4")
+plt.scatter(x[y_hc == 4, 0], x[y_hc == 4, 1], s = 100, c = "magenta", label="Cluster 5")
+plt.title("Clusters of Customers")
+plt.xlabel("Annual Income (k$)")
+plt.ylabel("Spendong Score (1 - 100)")
+plt.legend()
+plt.show()
