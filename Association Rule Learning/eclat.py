8dc4cad90f183e358e2530b3dfd6bba38be6ae04
Sherif Atitebi <sherifatitebi@gmail.com>
2022-02-07 10:49:00 +0000

    Eclat

--- Association Rule Learning/eclat.py
+++ Association Rule Learning/eclat.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Feb  6 16:01:50 2022
+Created on Mon Feb  7 10:17:26 2022
 
 @author: SHERIF ATITEBI O
 """
@@ -14,9 +14,8 @@ def inspect(results):
     lhs = [tuple(result[2][0][0])[0] for result in results]
     rhs = [tuple(result[2][0][1])[0] for result in results]
     supports = [result[1] for result in results]
-    confidences = [result[2][0][2] for result in results]
-    lifts = [result[2][0][3] for result in results]
-    return list(zip(lhs, rhs, supports, confidences, lifts))
+    
+    return list(zip(lhs, rhs, supports))
 
 #%%
 dataset = pd.read_csv("Market_Basket_Optimisation.csv", header=None)
@@ -26,15 +25,15 @@ for i in range(0, 7501):
     transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
 
 #%%
-#Training the Apriori dataset
+#Training the Eclat dataset
 rules = apriori(transactions = transactions, min_support=0.003, min_confidence=0.2, min_lift=3, min_length=2, max_length=2)
 
 #%%
 results = list(rules)
 #print(results)
 
-resultsinDataFrame = pd.DataFrame(inspect(results), columns = ['Left Hand Side', 'Right Hand Side', 'Support', 'Confidence', 'Lift'])
+resultsinDataFrame = pd.DataFrame(inspect(results), columns = ['Product 1', 'Product 2', 'Support'])
 print(resultsinDataFrame)
 
 #%%
-print(resultsinDataFrame.nlargest(n=10, columns="Lift"))
+print(resultsinDataFrame.nlargest(n=10, columns="Support"))
